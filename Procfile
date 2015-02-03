ac47d60a504e2e649e9b7159b622febdde87f77f
Andrew Hao <andrewhao@gmail.com>
2015-02-03 13:42:11 -0800

    Fixing John.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: shotgun lib/john.rb
--- lib/models/bathroom.rb
+++ lib/models/bathroom.rb
@@ -2,11 +2,26 @@ require 'net/http'
 require 'json'
 
 class Bathroom
+  ENDPOINT = 'http://192.168.7.40/port_3480/data_request?id=lu_status&DataVersion=131172371'
+  SENSOR_NAME = "urn:micasaverde-com:serviceId:SecuritySensor1"
+  TRIPPED = "Tripped"
+
   def self.occupied?
-    uri = URI.parse 'http://192.168.7.40/port_3480/data_request?id=lu_status&DataVersion=131172371'
+    uri = URI.parse(ENDPOINT)
     response = Net::HTTP.get_response(uri)
 
     json = JSON response.body
-    json['devices'][0]['states'][0]['value'] == '0'
+
+    devices_json = json['devices']
+
+    device = devices_json.detect do |d|
+      state = d["states"].first
+      state["service"] == SENSOR_NAME
+    end
+
+    device["states"].any? do |st|
+      st["variable"] == TRIPPED &&
+        st["value"] == "0"
+    end
   end
 end
